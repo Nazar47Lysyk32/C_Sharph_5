240b544b2c36e1e3fced55a05d37f91ec658269b
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-02-07 19:07:23 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть електронну адресу:");
+        string email = Console.ReadLine();
+
+        // Регулярний вираз для перевірки формату електронної пошти з доменом itstep.ua
+        string pattern = @"^[a-zA-Z0-9._%+-]+@itstep\.ua$";
+
+        // Перевірка за допомогою регулярного виразу
+        if (Regex.IsMatch(email, pattern))
+        {
+            Console.WriteLine("Електронна адреса має домен itstep.ua.");
+        }
+        else
+        {
+            Console.WriteLine("Електронна адреса не має домену itstep.ua.");
+        }
+    }
+}
