6629d13fad91a542aba08849a865df134177836a
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-02-07 19:09:29 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть український номер телефону:");
+        string phoneNumber = Console.ReadLine();
+
+        // Регулярний вираз для перевірки українського номера телефону
+        string pattern = @"^\+?38(0\d{2}|[3-9]\d{1})\d{7}$";
+
+        // Перевірка за допомогою регулярного виразу
+        if (Regex.IsMatch(phoneNumber, pattern))
+        {
+            Console.WriteLine("Введено коректний український номер телефону.");
+        }
+        else
+        {
+            Console.WriteLine("Некоректний український номер телефону.");
+        }
+    }
+}
